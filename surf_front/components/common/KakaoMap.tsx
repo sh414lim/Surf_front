611bfc6f4632af04874f49781c58cfe2da30b21c
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-01-10 19:53:45 +0900

    weather ui 위치 demo setting

--- surf_front/components/common/KakaoMap.tsx
+++ surf_front/components/common/KakaoMap.tsx
@@ -1,21 +1,58 @@
 import { grid } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { Grid, Box, Typography, Paper } from "@mui/material";
+import { WbSunny } from "@mui/icons-material";
+import axios from "axios";
 
-export default function KakaoMap() {
+//getServerSideProps api 통신 변환 예정
+// export async function getServerSideProps() {
+//   const res = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?lat=33&lon=126&appid=42ab373a0da8b86322e7d23c198bf55d`
+//   );
+//   const data = await res.json();
+
+//   return { props: { data } };
+// }
+
+function KakaoMap() {
   const [position, setPosition] = useState({
     lat: 33.5563,
     lng: 126.79581,
   });
+  const [weather, setWeather] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${position.lat}&lon=${position.lng}&appid=42ab373a0da8b86322e7d23c198bf55d`
+      )
+      .then((res) => {
+        console.log(res);
+        setWeather(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [position]);
+
+  console.log(weather);
 
   return (
     <>
-      <Grid spacing={2} sx={{ display: "flex" }}>
-        <Grid xs={6}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ display: "flex", justifyContent: "center", padding: "0px" }}
+      >
+        <Grid item xs={6}>
           <Map
             center={{ lat: position.lat, lng: position.lng }}
-            style={{ width: "50em", height: "50em" }}
+            style={{
+              width: "50em",
+              height: "54em",
+              padding: "0px 0px 0px 10px",
+              borderRadius: "20px",
+            }}
             onClick={(_t, mouseEvent) =>
               setPosition({
                 lat: mouseEvent.latLng.getLat(),
@@ -24,8 +61,184 @@ export default function KakaoMap() {
             }
           ></Map>
         </Grid>
-        {JSON.stringify(position)}
+        <Grid
+          sx={{
+            boxShadow: "10px 10px 10px 10px black",
+            marginTop: "20px",
+            borderRadius: "20px",
+            background: "#74b9ff",
+          }}
+          item
+          xs={5}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              borderBottom: "1px solid black",
+              padding: "0px 0px 100px 0px ",
+            }}
+          >
+            <WbSunny
+              sx={{
+                color: "yellow",
+                fontSize: "100px",
+                margin: "50px 0px 0px 30px",
+              }}
+            />
+            <Typography sx={{ margin: "70px 70px 0px 0px", fontSize: "30px" }}>
+              기온
+            </Typography>
+            <Typography sx={{ margin: "70px 70px 0px 0px", fontSize: "30px" }}>
+              습도
+            </Typography>
+            <Typography sx={{ margin: "70px 90px 0px 0px", fontSize: "50px" }}>
+              도시이름
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "8px 8px 8px 8px",
+              border: "1px solid black",
+              justifyContent: "space-around",
+              alignItems: "center",
+              margin: "15px 10px 15px 0px",
+            }}
+          >
+            <Typography>
+              <WbSunny
+                sx={{
+                  color: "yellow",
+                  fontSize: "50px",
+                }}
+              />
+            </Typography>
+            <Typography>기온:10 </Typography>
+            <Typography>습도:10 </Typography>
+            <Typography>파도높이:10 </Typography>
+            <Typography>강수량:10</Typography>
+          </Box>
+        </Grid>
+        {/* {JSON.stringify(weather)} */}
       </Grid>
     </>
   );
 }
+
+export default KakaoMap;
--- surf_front/pages/weather/weatherInfo.tsx
+++ surf_front/pages/weather/weatherInfo.tsx
@@ -6,24 +6,6 @@ export default function weatherInfo() {
   const [weather, setWeather] = useState();
   const [on, off] = useState(false);
 
-  useEffect(() => {
-    const lat: number = 58.7984;
-    const lng: number = 17.8081;
-    const params: string = "swellHeight";
-    fetch(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
-      {
-        headers: {
-          Authorization:
-            "6c3d6262-8667-11ed-bce5-0242ac130002-6c3d62da-8667-11ed-bce5-0242ac130002",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((jsonData) => {
-        setWeather(jsonData);
-      });
-  }, [on]);
   console.log(weather);
 
   return (
@@ -32,3 +14,5 @@ export default function weatherInfo() {
     </>
   );
 }
+
+// 42ab373a0da8b86322e7d23c198bf55d
