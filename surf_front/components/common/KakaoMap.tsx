7d8ee615d3c6084ee8719204f3bb6e71acff0182
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-01-10 17:27:18 +0900

    카카오 맵 위도 경도 검색 추가

--- surf_front/components/common/KakaoMap.tsx
+++ surf_front/components/common/KakaoMap.tsx
@@ -1,19 +1,30 @@
 import { grid } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { Grid, Box, Typography, Paper } from "@mui/material";
 
 export default function KakaoMap() {
+  const [position, setPosition] = useState({
+    lat: 33.5563,
+    lng: 126.79581,
+  });
+
   return (
     <>
       <Grid spacing={2} sx={{ display: "flex" }}>
         <Grid xs={6}>
           <Map
-            center={{ lat: 33.5563, lng: 126.79581 }}
+            center={{ lat: position.lat, lng: position.lng }}
             style={{ width: "50em", height: "50em" }}
+            onClick={(_t, mouseEvent) =>
+              setPosition({
+                lat: mouseEvent.latLng.getLat(),
+                lng: mouseEvent.latLng.getLng(),
+              })
+            }
           ></Map>
         </Grid>
-        <Grid xs={6}></Grid>
+        {JSON.stringify(position)}
       </Grid>
     </>
   );
